8fff49162c5b73c527ae2c3a378622c3e23f3918
Sudeep Ghosh <133862916+SudeepGhosh12@users.noreply.github.com>
2025-04-27 09:17:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+const buttons = document.getElementById('buttons');
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button');
+  btn.classList.add('btn');
+  btn.innerText = sound;
+
+  btn.addEventListener('click', () => {
+    stopSounds();
+    const audio = document.getElementById(sound);
+    audio.play();
+  });
+
+  buttons.appendChild(btn);
+
+  const audio = document.createElement('audio');
+  audio.src = `sounds/${sound}.mp3`;
+  audio.id = sound;
+  document.body.appendChild(audio);
+});
+
+// STOP BUTTON
+const stopBtn = document.createElement('button');
+stopBtn.classList.add('stop');
+stopBtn.innerText = 'stop';
+
+stopBtn.addEventListener('click', stopSounds);
+
+buttons.appendChild(stopBtn);
+
+// Function to stop all sounds
+function stopSounds() {
+  sounds.forEach(sound => {
+    const audio = document.getElementById(sound);
+    audio.pause();
+    audio.currentTime = 0;
+  });
+}
